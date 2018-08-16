d9001baf3e86c80f4d2f53806b9abc915438b6e4
Tomasz Trzcinski <tomasz.trzcinski@capgemini.com>
2018-08-16 13:41:44 +0200

    Added process BrocasArea.method.

--- Brain/BrocasArea.cs
+++ Brain/BrocasArea.cs
@@ -8,5 +8,20 @@ namespace Lycopersicon_src.Brain
         public string respondTo(string phrase) {
             return "Tomato";
         }
+
+        /// <summary>
+        /// Processes given phrase in order to obtain meaning for better response.
+        /// </summary>
+        /// <param name="phrase">recorded phrase</param>
+        /// <returns>analysis report</returns>
+        public string process(string phrase)
+        {
+            //Split into lines
+            var lines = phrase.Split(".!;?");
+
+            var words = phrase.Split(".!;? /t/r/n");
+
+            return $"Phrase contains {lines.Length} lines and {words.Length} words.";
+        }
     }
 }
