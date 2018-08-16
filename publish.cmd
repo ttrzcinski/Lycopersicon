d90d3d9f13cb48632302a4d6bba25b038e623887
Tomasz Trzcinski <tomasz.trzcinski@capgemini.com>
2018-08-16 10:44:46 +0200

    Freshly generated from Azure Portal.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild AspNetCore-EchoBot-With-State.sln -p:DeployOnBuild=true -p:PublishProfile=lycopersicon-Web-Deploy.pubxml -p:Password=BhuBMt5RyYG3blTWEvfvbpqADgzctr4RPQeu4RBTpKnRJPnEv8SDNZhmdRxs
+
